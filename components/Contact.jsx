5106751be6d1287ea53bea8b43c3c8cb4d61c514
Moghadam80 <etehadian6@gmail.com>
2024-01-29 22:11:09 +0330

    Add deploy

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -5,7 +5,7 @@ import { AiOutlineMail } from 'react-icons/ai';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 import { HiOutlineChevronDoubleUp } from 'react-icons/hi';
-import ContactImg from '../public/assets/contact.jpg';
+// import ContactImg from '/public/assets/contact.jpg';
 
 const Contact = () => {
   return (
@@ -22,7 +22,7 @@ const Contact = () => {
               <div>
                 <Image
                   className='rounded-xl hover:scale-105 ease-in duration-300'
-                  src={ContactImg}
+                  src={'/assets/contact.jpg'}
                   alt='/'
                 />
               </div>
